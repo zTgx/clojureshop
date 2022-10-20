4e24e04b6e7c9da8108a247dcf289ba90408b44e
zTgx <747674262@qq.com>
2022-10-20 19:56:51 +0800

    init clojure shop

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojureshop
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojureshop/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojureshop.core-test
+  (:require [clojure.test :refer :all]
+            [clojureshop.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
